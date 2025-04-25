920e1fb18628ad980548e4e968ef1f801da0ca95
SUSBOY97 <guuhsykes@outlook.com>
2025-04-24 21:40:05 -0300

    testeCaraio

--- cadastro.js
+++ cadastro.js
@@ -7,7 +7,6 @@ async function cadastro()
    let Email = document.getElementById("email").value;
    let senha = document.getElementById("senha").value;
    let Celular = document.getElementById("Celular").value;
-
    let url = "https://go-wash-api.onrender.com/api/user";
 
     let parametros =
